d3a974091263140e88d0d4ca7d37195794911012
João Gomes <joaogomesdev32@gmail.com>
2026-05-22 18:56:00 +0100

    :sparkles: add ft_strjoin

--- get-next_line_utils.c
+++ get-next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 18:17:05 by joaog             #+#    #+#             */
-/*   Updated: 2026/05/22 18:29:20 by joaog            ###   ########.fr       */
+/*   Updated: 2026/05/22 18:53:33 by joaog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
+	c = '\n';
 	i = 0;
 	while (s[i])
 	{
@@ -36,4 +37,33 @@ char	*ft_strchr(char *s, int c)
 	if ((char)c == '\0')
 		return (&s[i]);
 	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*result;
+	int		i;
+
+	if (!s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	result = malloc(len_s1 + len_s2 + 1);
+	if (!result)
+		return (NULL);
+	while (s1[i])
+	{
+		result[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (s2[i] && (len_s1 + 1))
+	{
+		result[len_s1 + i] = s2[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
 }
