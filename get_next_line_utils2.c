1d95ca77312a865579c347b8c5fc50d2335daef6
João Gomes <joaogomesdev32@gmail.com>
2026-05-24 12:35:10 +0100

    :sparkles: introduce update_leftover

--- get_next_line_utils2.c
+++ get_next_line_utils2.c
@@ -6,7 +6,7 @@
 /*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/23 13:05:53 by joaog             #+#    #+#             */
-/*   Updated: 2026/05/23 14:09:50 by joaog            ###   ########.fr       */
+/*   Updated: 2026/05/24 12:25:16 by joaog            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,20 @@ char	*extract_line(char *letfover)
 	else
 		len = ft_strlen(letfover);
 	return (ft_substr(letfover, 0, len));
+}
+
+char	*update_leftover(char *leftover)
+{
+	char	*newline;
+	char	*rest;
+
+	newline = ft_strchr(leftover, '\n');
+	if (!leftover || !newline[1])
+	{
+		free(leftover);
+		return (NULL);
+	}
+	rest = ft_strdup(newline + 1);
+	free(leftover);
+	return rest;
 }
