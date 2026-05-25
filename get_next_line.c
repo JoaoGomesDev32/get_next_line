db9a9b825b9c0b491f3f12415b06ab0bd2a5feda
Joao Felipe Gomes Da Silva <joagomes@c1r5s5.42lisboa.com>
2026-05-25 15:40:43 +0100

    :art: change structure

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,72 @@
 /*   By: joagomes <joagomes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/25 11:16:49 by joagomes          #+#    #+#             */
-/*   Updated: 2026/05/25 11:25:19 by joagomes         ###   ########.fr       */
+/*   Updated: 2026/05/25 15:36:01 by joagomes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*read_collect_leftover(int fd, char *leftover)
+{
+	char	*buffer;
+	int		bytes_read;
+	char	*joined;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	while (1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+			break ;
+		buffer[bytes_read] = '\0';
+		joined = ft_strjoin(leftover, buffer);
+		free(leftover);
+		leftover = joined;
+		if (ft_strchr(leftover, '\n'))
+			break ;
+	}
+	free(buffer);
+	if (bytes_read == -1)
+	{
+		free(leftover);
+		return (NULL);
+	}
+	return (leftover);
+}
+
+char	*extract_line(char *letfover)
+{
+	char	*newline;
+	size_t	len;
+
+	if (!letfover || !letfover[0])
+		return (NULL);
+	newline = ft_strchr(letfover, '\n');
+	if (newline)
+		len = (newline - letfover) + 1;
+	else
+		len = ft_strlen(letfover);
+	return (ft_substr(letfover, 0, len));
+}
+
+char	*update_leftover(char *leftover)
+{
+	char	*newline;
+	char	*rest;
+
+	newline = ft_strchr(leftover, '\n');
+	if (!newline || !newline[1])
+	{
+		free(leftover);
+		return (NULL);
+	}
+	rest = ft_strdup(newline + 1);
+	free(leftover);
+	return (rest);
+}
 
 char	*get_next_line(int fd)
 {
